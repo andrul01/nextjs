e27f4016d1cc786772436d1728ba3e49bf646a36
Andrul01 <andrumorallies@gmail.com>
2025-02-01 18:07:08 +0530

    added new scroll sticky section

--- biggner/src/app/page.tsx
+++ biggner/src/app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "@/components/HeroSection";
 import LatestBlogs from "@/components/LatestBlogs";
+import WhyJoinUs from "@/components/WhyJoinUs";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.35]">
         <HeroSection/>
         <LatestBlogs/>
+        <WhyJoinUs/>
     </main>
   );
 }
--- biggner/src/components/LatestBlogs.tsx
+++ biggner/src/components/LatestBlogs.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { Button } from "./ui/moving-border";
 import postData from "../data/post.json";
 import { BackgroundGradient } from './ui/background-gradient';
+import Image from 'next/image';
 
 interface Posts {
   id:number,
@@ -19,14 +20,14 @@ function LatestBlogs() {
   const featuredPosts = postData.posts.filter((posts:Posts) => posts.isFeatured)
 
   return (
-    <div className="mt-3 py-10 bg-gray-200 ">
+    <div className="mt-3 py-5 bg-gray-200 ">
       <div>
         <div className="text-center text-black">
           <h2 className="text-4xl font-bold font-serif">LATEST BLOG POST</h2>
           <p className="mt-4 font-mono">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem, quasi!</p>
         </div>
       </div>
-      <div className='mt-10'>2
+      <div className='mt-5'>2
         <div className='grid grid-cols-1 md:grid-cols-3 gap-4 justify-center'>
           {featuredPosts.map((posts:Posts) => (
               <div
@@ -34,10 +35,18 @@ function LatestBlogs() {
                 className='text-black flex justify-center' 
               >
                 <BackgroundGradient
-                  className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900"
+                  className="rounded-[22px] max-w-sm p-3 sm:p-10 bg-white dark:bg-zinc-900"
                 >
-                  <div className='text-center text-white'>
-                    <p className='text-3xl font-serif mb-5'>{posts.title}</p>
+                  <div className='text-center text-white flex flex-col items-center'>
+                    <p className='text-3xl font-serif mb-2'>{posts.title}</p>
+                    <Image 
+                      className='border rounded-lg my-3'
+                      src="/fnspidy.png"
+                      width={500}
+                      height={50}
+                      alt='fnspidy image'
+                    
+                    />
                     <p className='font-mono mb-5'>{posts.content}</p>
                     <Link 
                       href={`/posts/${posts.slug}`}
@@ -51,7 +60,7 @@ function LatestBlogs() {
         </div>
 
       </div>
-      <div className="text-center mt-10">
+      <div className="text-center mt-5">
         <Link href={'/'}>
           <Button 
             borderRadius='2rem'
